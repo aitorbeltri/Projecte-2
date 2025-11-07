d40c3e857aba8f9732afeae7528b264324f3bc1c
aitorbeltri <aitorbeltri@iesmontsia.org>
2025-11-07 17:25:25 +0100

    ejercicis 4

--- Training4.1.py
+++ Training4.1.py
@@ -1,5 +1,4 @@
-if __name__ == "__main__":
-    x = 0
-    while x < 201:
+x = 0
+while x < 201:
         print(x)
         x += 2
--- Training4.3.py
+++ Training4.3.py
@@ -1,20 +1,15 @@
-def numeros(num):
-    while True:
-        try:
-            return float(input(f"Escriu el nombre {num+1}/10: "))
-        except ValueError:
-            print("Entrada no vàlida. Torna-ho a intentar.")
-
-def main():
-    negatiu = False
-    for i in range(10):
-        n = numeros(i)
-        if n < 0:
-            negatiu = True
-    if negatiu:
-        print("hi havia almenys un nombre negatiu")
-    else:
-        print("no hi ha cap nombre negatiu")
-
-if __name__ == "__main__":
-    main()
+n_negatiu = False
+nombre = 0
+num_vegades = 0
+while num_vegades < 10:
+    try:
+        nombre = int(input("Introdueix un nombre: "))
+        if nombre < 0:
+            n_negatiu = True
+        num_vegades += 1
+    except ValueError:
+        print("Introdueix un número vàlid.")
+if n_negatiu:
+    print("S'ha introduït un nombre negatiu.")
+else:
+    print("No hi ha cap negatiu.")
